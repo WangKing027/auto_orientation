e94de5ba238089bf235b53e73c4c06073e2d7dde
WangKing027 <1970562876@qq.com>
2020-07-03 16:01:06 +0800

    Update AutoOrientationPlugin.java
    兼容 v2 包功能

--- android/src/main/java/de/bytepark/autoorientation/AutoOrientationPlugin.java
+++ android/src/main/java/de/bytepark/autoorientation/AutoOrientationPlugin.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -11,58 +15,78 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** AutoOrientationPlugin */
-public class AutoOrientationPlugin implements MethodCallHandler {
-  Activity activity;
+public class AutoOrientationPlugin implements FlutterPlugin, MethodCallHandler {
 
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "auto_orientation");
-    channel.setMethodCallHandler(new AutoOrientationPlugin(registrar.activity()));
-  }
+    private Activity activity;
+    private MethodChannel methodChannel ;
 
-  private AutoOrientationPlugin(Activity activity) {
-    this.activity = activity;
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    switch(call.method) {
-      case "setLandscapeRight":
-        this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        break;
-      case "setLandscapeLeft":
-        this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
-        break;
-      case "setPortraitUp":
-        this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        break;
-      case "setPortraitDown":
-        this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
-        break;
-      case "setPortraitAuto":
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
-        } else {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
-        }
-        break;
-      case "setLandscapeAuto":
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
-        } else {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
-        }
-        break;
-      case "setAuto":
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_USER);
-        } else {
-          this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
-        }
-        break;
-      default:
-        result.notImplemented();
-        break;
+    /** Plugin registration. */
+    public static void registerWith(Registrar registrar) {
+        final AutoOrientationPlugin instance = new AutoOrientationPlugin(registrar.activity());
+        instance.onAttachedToEngine(registrar.messenger());
     }
-  }
+
+    private AutoOrientationPlugin(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        switch(call.method) {
+            case "setLandscapeRight":
+                this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+            case "setLandscapeLeft":
+                this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                break;
+            case "setPortraitUp":
+                this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+            case "setPortraitDown":
+                this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                break;
+            case "setPortraitAuto":
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
+                } else {
+                    this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+                }
+                break;
+            case "setLandscapeAuto":
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
+                } else {
+                    this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+                }
+                break;
+            case "setAuto":
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_USER);
+                } else {
+                    this.activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+                }
+                break;
+            default:
+                result.notImplemented();
+                break;
+        }
+    }
+
+    private void onAttachedToEngine(BinaryMessenger messenger) {
+        methodChannel = new MethodChannel(messenger, "auto_orientation");
+        methodChannel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        onAttachedToEngine(flutterPluginBinding.getBinaryMessenger());
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null ;
+        activity = null ;
+    }
+
 }
